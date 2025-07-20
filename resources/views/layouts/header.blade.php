bffe5bf5ec284e43134f20b1fd4ebf38a2fa5452
mijashsunar10 <mijashsunar.xdezo@gmail.com>
2025-07-20 15:33:40 +0545

    Frontend inserted

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -52,22 +52,22 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <div class="navbar-nav ms-auto py-0">
-                <a href="index.html" class="nav-item nav-link active">Home</a>
-                <a href="about.html" class="nav-item nav-link">About</a>
-                <a href="service.html" class="nav-item nav-link">Service</a>
+                <a href="{{route('index')}}" class="nav-item nav-link active">Home</a>
+                <a href="{{route('about')}}" class="nav-item nav-link">About</a>
+                <a href="{{route('service')}}" class="nav-item nav-link">Service</a>
                 <div class="nav-item dropdown">
                     <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
                     <div class="dropdown-menu m-0">
-                        <a href="price.html" class="dropdown-item">Pricing Plan</a>
-                        <a href="team.html" class="dropdown-item">Our Dentist</a>
-                        <a href="testimonial.html" class="dropdown-item">Testimonial</a>
-                        <a href="appointment.html" class="dropdown-item">Appointment</a>
+                        <a href="{{route('price')}}" class="dropdown-item">Pricing Plan</a>
+                        <a href="{{route('team')}}" class="dropdown-item">Our Dentist</a>
+                        <a href="t{{route('testimonial')}}" class="dropdown-item">Testimonial</a>
+                        <a href="{{route('appointment')}}" class="dropdown-item">Appointment</a>
                     </div>
                 </div>
-                <a href="contact.html" class="nav-item nav-link">Contact</a>
+                <a href="{{route('contact')}}" class="nav-item nav-link">Contact</a>
             </div>
             <button type="button" class="btn text-dark" data-bs-toggle="modal" data-bs-target="#searchModal"><i class="fa fa-search"></i></button>
-            <a href="appointment.html" class="btn btn-primary py-2 px-4 ms-3">Appointment</a>
+            <a href="{{route('appointment')}}" class="btn btn-primary py-2 px-4 ms-3">Appointment</a>
         </div>
     </nav>
     <!-- Navbar End -->
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\AppointmentController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PriceController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\TeamController;
+use App\Http\Controllers\TestimonialController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,9 +26,28 @@ Route::middleware('auth')->group(function () {
 });
 
 
-Route::get('/index', function () {
-    return view('frontend.home.index');
-})->name('index');
+// Route::get('/index', function () {
+//     return view('frontend.home.index');
+// })->name('index');
 
+
+Route::get('/index',[HomeController::class, 'index'])->name('index');
+
+Route::get('/about',[AboutController::class, 'index'])->name('about');
+
+Route::get('/service',[ServiceController::class,'index'])->name('service');
+
+Route::get('/contact',[ContactController::class,'index'])->name('contact');
+
+Route::get('/appointment',[AppointmentController::class,'index'])->name('appointment');
+
+Route::get('/price',[PriceController::class,'index'])->name('price');
+
+Route::get('/testimonials',[TestimonialController::class,'index'])->name('testimonial');
+
+
+Route::get('/team',[TeamController::class,'index'])->name('team');
+
+// Route::get('/team',)
 
 require __DIR__.'/auth.php';
